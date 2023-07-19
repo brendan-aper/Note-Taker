e36e4bd9e1cf13ee429dc98aacd8864087c8d3c2
Brendan Aper <brendanaper@gmail.com>
2023-07-18 19:46:33 -0700

    refactor

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const htmlRoutes = require('./routes/html-routes');
+const apiRoutes = require('./routes/api-routes')
+
+app.use(express.urlencoded({ extended: false}));
+app.use(express.json());
+app.use(express.static('public'));
+app.use(htmlRoutes);
+app.use(apiRoutes);
 
 app.listen(PORT, () =>
   console.log(`Server listening at http://localhost:${PORT}`)
