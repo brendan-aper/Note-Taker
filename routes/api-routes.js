ea80180d59eb1a3dbdfeb9b22c1d2e702ae2c57c
Brendan Aper <brendanaper@gmail.com>
2023-07-19 17:45:14 -0700

    updated better syntax after looking at documentation

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const uuid = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 const fs = require('fs');
 
 router.get('/api/notes', async (req, res) => {
@@ -12,7 +12,7 @@ router.post('/api/notes', (req, res) => {
   const newPost = {
     title: req.body.title,
     text: req.body.text,
-    id: uuid(),
+    id: uuidv4(),
   };
   dbJson.push(newPost);
   fs.writeFileSync('db/db.json', JSON.stringify(dbJson));
